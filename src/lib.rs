7cbe09b312a4adf6ab08d2ac5083888fc4569ae4
Paolo Barbolini <paolo@paolo565.org>
2022-06-08 16:53:57 +0200

    docs: deny broken_intra_doc_links

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@
 //! [lettre]: https://crates.io/crates/lettre
 
 #![forbid(unsafe_code)]
-#![deny(rust_2018_idioms, missing_docs)]
+#![deny(rust_2018_idioms, missing_docs, rustdoc::broken_intra_doc_links)]
 
 pub mod body;
 pub mod headers;
